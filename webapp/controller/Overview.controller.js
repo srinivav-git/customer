db9adcae836e0c9ecd8a1057d91c7363440200dd
Ven Srinivasan <c_ven.srinivasan@solidpowerbattery.com>
2025-08-16 21:01:01 +0000

    commit_main

--- webapp/controller/Overview.controller.js
+++ webapp/controller/Overview.controller.js
@@ -1,10 +1,17 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
+], (Controller,JSONModel) => {
     "use strict";
 
     return Controller.extend("com.sp.cust.customer.controller.Overview", {
         onInit() {
+            let oModel = new JSONModel( {
+                firstname: "MaxBulla",
+                age: 27,
+                iseditable : true
+            })
+            this.getView().setModel(oModel);
         }
     });
 });
